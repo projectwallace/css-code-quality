a7932c12fd1f21200b6c1b5e33bd3a943cf18e1c
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2025-04-11 16:07:11 +0200

    more fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { resolve } from "path"
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
 
 export default defineConfig({
 	build: {
 		lib: {
-			entry: resolve(__dirname, "src/index.js"),
-			name: "cssCodeQuality",
-			fileName: "css-code-quality",
-			formats: ['es']
+			entry: [
+				'./src/index.js',
+				'./src/core.js',
+			],
+			fileName: () => `[name].js`,
+			formats: ["es"],
 		},
 		rollupOptions: {
 			// make sure to externalize deps that shouldn't be bundled
